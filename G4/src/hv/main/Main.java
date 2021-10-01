f1f4e9ee11bebcb47f6b4135293a7d859d4685be
heinarva <heinarva@netscape.net>
2021-10-01 13:46:57 +0300

    simplest java application

---
+++ G4/src/hv/main/Main.java
@@ -0,0 +1,24 @@
+package hv.main;
+
+public class Main {
+
+
+
+	
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater  (new Runnable(){
+
+	//		int num1, num2 = 10;
+			
+			public void run() {
+				
+	//			Mgr mgr = new Mgr();
+	//			mgr.start();
+				
+				System.out.println("The eagle has landed!!");
+				
+				}
+			});
+		}
+	
+	}
